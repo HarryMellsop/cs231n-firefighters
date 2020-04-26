5af54768498797760e939b6a05a6782f253c999c
Harry Mellsop <harrymellsop@gmail.com>
2020-04-27 11:37:34 +1200

    Assorted

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import ee
 
 ee.Initialize()
 
-image = ee.Image('srtm90_v4')
-print(image.getInfo())
+image1 = ee.Image('srtm90_v4')
+path = image1.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': '[[-120, 35], [-119, 35], [-119, 34], [-120, 34]]'
+})
+print(path)
