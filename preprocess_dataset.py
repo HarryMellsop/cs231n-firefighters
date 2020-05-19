c9e62b4c1c2ed2e21a948d9928e0ffaf08c19307
Harry Mellsop <harrymellsop@gmail.com>
2020-05-19 17:09:44 +1200

    Moved code into the preprocess_dataset file

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,52 +1,47 @@
 import sqlite3
 import pandas as pd
 import numpy as np
-import colorcet as cc
-from bokeh.io import output_notebook
-from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, LogColorMapper
+import pickle
+import ee
 
+# given a range of latitudes and longitudes, a fire database dataframe, and a desired resolution, this function
+# will linearly partition the geodata into smaller grids
+def extract_localized_wildfires(minLat, maxLat, minLong, maxLong, df, lat_resolution=None, long_resolution=None, resolution=None):
+    import numpy as np
+    
+    assert (lat_resolution != None and long_resolution != None) or resolution != None
+    if resolution != None:
+        long_resolution = resolution
+        lat_resolution = resolution
+
+    longitude_range = np.linspace(maxLong, minLong, long_resolution + 1)
+    latitude_range = np.linspace(minLat, maxLat, lat_resolution + 1)
+
+    datapoints = {}
+    for row in range(resolution):
+        for col in range(resolution):
+            rel = df[df["LATITUDE"] >= latitude_range[row]]
+            rel = rel[rel["LATITUDE"] < latitude_range[row + 1]]
+            rel = rel[rel["LONGITUDE"] <= longitude_range[col]]
+            rel = rel[rel["LONGITUDE"] > longitude_range[col + 1]]
+            datapoints[(row, col)] = rel
+
+    return datapoints
+
+# asyncronously initialize the earth engine
+ee.Initialize()
+
+# load the initial fire dataset
 cnx = sqlite3.connect('./us_wildfire_dataset/FPA_FOD_20170508.sqlite')
-df = pd.read_sql_query("SELECT LATITUDE, LONGITUDE, FIRE_SIZE, STATE FROM fires", cnx)
-df.head(5)
+df = pd.read_sql_query("SELECT DISCOVERY_DATE, LATITUDE, LONGITUDE, FIRE_SIZE, STATE FROM fires", cnx)
 
-pd.options.mode.chained_assignment = None
+# constrain the searches to California, and fix the date/time format
+df = df[df["STATE"] == "CA"]
+df['Date'] = pd.to_datetime(df['DISCOVERY_DATE'], unit='D', origin='julian')
+df = df.drop("DISCOVERY_DATE", 1)
 
-new = df.loc[(df.loc[:,'STATE']!='AK') & (df.loc[:,'STATE']!='HI') & (df.loc[:,'STATE']!='PR')]
+print(extract_localized_wildfires(34, 36, -118, -116, df, resolution=15)[(0, 2)])
 
-new.loc[:,'LATITUDE'] = ((new.loc[:,'LATITUDE']*10).apply(np.floor))/10
-new.loc[:,'LONGITUDE'] = ((new.loc[:,'LONGITUDE']*10).apply(np.floor))/10
-new.loc[:,'LL_COMBO'] = new.loc[:,'LATITUDE'].map(str) + '-' + new.loc[:,'LONGITUDE'].map(str)
-grouped = new.groupby(['LL_COMBO', 'LATITUDE', 'LONGITUDE'])
-
-number_of_wf = grouped['FIRE_SIZE'].agg(['count']).reset_index()
-number_of_wf.head(5)
-
-size_of_wf = grouped['FIRE_SIZE'].agg(['mean']).reset_index()
-size_of_wf.head(5)
-
-source = ColumnDataSource(number_of_wf)
-p1 = figure(title="Number of wildfires occurring from 1992 to 2015 " + \
-            "(lighter color means more wildfires)",
-           toolbar_location=None, plot_width=600, plot_height=400)
-p1.background_fill_color = "black"
-p1.grid.grid_line_color = None
-p1.axis.visible = False
-color_mapper = LogColorMapper(palette=cc.fire)
-glyph = p1.circle('LONGITUDE', 'LATITUDE', source=source,
-          color={'field': 'count', 'transform' : color_mapper},
-          size=1)
-output_notebook()
-show(p1)
-
-source = ColumnDataSource(size_of_wf)
-p2 = figure(title="Average size of wildfires occurring from 1992 to 2015 " + \
-            "(lighter color means bigger fire)",
-           toolbar_location=None, plot_width=600, plot_height=400)
-p2.background_fill_color = "black"
-p2.grid.grid_line_color = None
-p2.axis.visible = False
-glyph = p2.circle('LONGITUDE', 'LATITUDE', source=source,
-          color={'field': 'mean', 'transform' : color_mapper},
-          size=1)
-show(p2)
+# # save the dataframe to disk
+# with open("./us_wildfire_dataset/ca_fires_raw.pkl", "wb") as f:
+#     pickle.dump(df, f, protocol=pickle.HIGHEST_PROTOCOL)
