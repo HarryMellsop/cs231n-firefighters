0763f02491fd43533f628563a8a40555ebd5ba93
Harry Mellsop <harrymellsop@gmail.com>
2020-05-20 11:20:53 +1200

    Saving datastructures completed

--- data_processing/create_datasets_master.py
+++ data_processing/create_datasets_master.py
@@ -1,5 +1,6 @@
 from download_images import download_images_main
 from create_labelled_grid import create_grid_main
+import pickle
 
 # let's define the area to examine
 minLong = -118.0
@@ -16,3 +17,10 @@ y = create_grid_main()
 
 # pull images off of Google Earth Engine to create an 'X' tensor that will be (100x100) x IMG_HEIGHT x IMG_WIDTH x NUM_CHANNELS
 X = download_images_main()
+
+# save these two databases for use by a later ML model
+with open("./us_wildfire_dataset/ca_fires_X.pkl", "wb") as f:
+    pickle.dump(X, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open("./us_wildfire_dataset/ca_fires_y.pkl", "wb") as f:
+    pickle.dump(y, f, protocol=pickle.HIGHEST_PROTOCOL)
